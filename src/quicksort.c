0d007ab9063dc7b83ee2011d5b40a390866e1dc1
Peter Janovsky <pjanovsky@twilio.com>
2017-01-17 13:48:59 -0800

    Quicksort Source Cleanup

--- src/quicksort.c
+++ src/quicksort.c
@@ -47,28 +47,22 @@ int partition(int *a, int p, int r)
 {
     // value at end of subarray offset
     int x = a[r-1];
-printf("x: %d\n", x);
 
     // offset immediately before beginning of subarray
     int i = p - 1;
-printf("i: %d\n", i);
 
     // traverse subarray, stopping at the offset prior to the end
     for ( int j=p; j < (r-1); j++ ) {
-printf("j: %d a[%d]: %d\n", j, j, a[j]);
         if ( a[j] <= x ) {
-            /* the offset's value is less than the subarray's ending offset's value
-             * exchange
+            /* exchange the offset's value is less than the subarray's ending
+             * offset's value
              */
             i = i + 1;
-printf("exchanging a[%d]: %d with a[%d]: %d\n", i+1, a[i], j, a[j]);
             exchange(a, i, j);
         }
     }
 
-printf("exchanging a[%d]: %d with a[%d]: %d\n", i+1, a[i+1], r, a[r]);
     exchange(a, (i + 1), (r - 1));
-
     return (i + 1);
 }
 
@@ -86,17 +80,10 @@ printf("exchanging a[%d]: %d with a[%d]: %d\n", i+1, a[i+1], r, a[r]);
  */
 void quicksort(int *a, int p, int r, char *str)
 {
-printf("quicksort called with p: %d r: %d on %s\n", p, r, str);
     // process when the beginning subarray offset is less than the ending
     // subarray offset. otherwise, this subarray has been sorted
     if ( p < r ) {
         int q = partition(a, p, r);
-
-printf("partition called with p: %d r: %d\n", p, r);
-for( int i=0; i < 8; i++ )
-    printf("a[%d]: %d    ", i, a[i]);
-printf("\n");
-
         quicksort(a, p, (q - 1), "left");
         quicksort(a, (q + 1), r, "right");
     }
