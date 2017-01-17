1c76999ff0a544cd281b23f73d5e12a57df4b3cd
Peter Janovsky <pjanovsky@twilio.com>
2017-01-17 13:34:42 -0800

    Insertion Sort Comments

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,12 +1,20 @@
 /*
+ * Function: insertion_sort
+ * ------------------------
+ * sorts an array by traversing in opposing directions, transposing the value
+ * at the offset of the loop advancing towards the end of the array with the
+ * value at the offset of the loop advancing towards the beginning of the array
+ * if it greater than the value to which it is being compared
+ *
  * time complexity
  *   c1 n squared
  *     where c1 is a constant that does not depend on n
  *     takes time roughly proportional to n squared
  *
- * @param sortable array to sort
- * @param size size of sortable array
- * @return the sorted array
+ * sortable: array to sort ( signed integers )
+ * size: size of sortable array
+ *
+ * returns: the sorted array
  */
 int *insertion_sort(int sortable[], int size)
 {
@@ -27,7 +35,7 @@ int *insertion_sort(int sortable[], int size)
            *  from right-to-left
            */
           int i = j - 1;
-          while ( i > -1 && sortable[i] > key ) {
+          while ( (i > -1) && (sortable[i] > key) ) {
 
                sortable[i + 1] = sortable[i];
                i = i - 1;
